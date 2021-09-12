052942e566977d23093207db5e65f67b2a7df942
unknown <sumin9278@soongsil.ac.kr>
2021-09-12 16:51:35 +0900

    2021.09.12

--- container/Home.js
+++ container/Home.js
@@ -3,6 +3,18 @@ import {HomeWrapper} from "../helper/StyleUtil";
 import LoginService from "../helper/LoginService";
 import AnnouncementList from "../helper/AnnouncementList";
 
+const BACHELOR = "/ssuzalal/bachelor.do";
+const SCHOLARSHIP = "/ssuzalal/scholarship.do";
+const EXCHANGE = "/ssuzalal/exchange.do";
+const FOREIGNER = "/ssuzalal/foreigner.do";
+const RECRUIT = "/ssuzalal/recruit.do";
+const EVENTS = "/ssuzalal/events.do";
+const TEACHER_RECRUIT = "/ssuzalal/teacher_recruit.do";
+const TEACHING_PROFESSION = "/ssuzalal/teaching_profession.do";
+const VOLUNTEER = "/ssuzalal/volunteer.do";
+const ETC = "/ssuzalal/etc.do";
+const COVID19 = "/ssuzalal/covid19.do";
+
 const Home = () => {
     const [id, setId] = useState('NONE');
     const [time, setTime] = useState('2357-11-13');
@@ -23,11 +35,21 @@ const Home = () => {
 
     return(
         <HomeWrapper>
-            <h1>로그인 완료.</h1>
-            <h3>Welcome on board! {id}</h3>
+            <h2>로그인 완료.</h2>
+            <h1>환영합니다. {id}</h1>
             <h4>시간 : {time}</h4>
             <h5>이름 : {username} 학부 : {major}</h5>
-            <AnnouncementList />
+            <AnnouncementList url={BACHELOR}/>
+            <AnnouncementList url={SCHOLARSHIP}/>
+            <AnnouncementList url={EXCHANGE}/>
+            <AnnouncementList url={FOREIGNER}/>
+            <AnnouncementList url={RECRUIT}/>
+            <AnnouncementList url={EVENTS}/>
+            <AnnouncementList url={TEACHER_RECRUIT}/>
+            <AnnouncementList url={TEACHING_PROFESSION}/>
+            <AnnouncementList url={VOLUNTEER}/>
+            <AnnouncementList url={ETC}/>
+            <AnnouncementList url={COVID19}/>
         </HomeWrapper>
     );
 }
--- helper/AnnouncementList.js
+++ helper/AnnouncementList.js
@@ -2,26 +2,41 @@ import React, {useState, useEffect} from 'react';
 import AnnouncementService from "./AnnouncementService";
 import {Ul, Li, Button} from './StyleUtil';
 import {Link} from "react-router-dom";
+import styled from "styled-components";
 
-const BACHELOR = "/ssuzalal/bachelor.do";
+const Fieldset = styled.fieldset`
+    display: flex;
+    flex-direction: column;
+`;
 
-const AnnouncementList = () => {
+const Legend = styled.legend`
+    display: flex;
+    flex-direction: column;
+    font-size: 20px;
+`;
+
+const AnnouncementList = props => {
     const [list, setList] = useState(new Map());
+    const [type, setType] = useState('');
 
     useEffect(() => {
-        AnnouncementService.getAnnouncement(BACHELOR).then(result => {
+        AnnouncementService.getAnnouncement(props.url).then(result => {
             setList(result.map((item) => <Li>
                 {item.announcedDate}&nbsp;{item.isCompleted ? "[완료]" : "[진행]"}&nbsp;{item.title}&nbsp;<a target="_blank" href={item.link}>바로가기</a>
             </Li>));
+            setType(result[0].announcedType);
             }).catch(err => {
                 console.log(err);
             });
         }, []);
 
     return (
-        <Ul>
-            {list}
-        </Ul>
+        <Fieldset>
+            <Legend>{type}</Legend>
+            <Ul>
+                {list}
+            </Ul>
+        </Fieldset>
     );
 }
 
